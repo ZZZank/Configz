2cb79ffe62ffa4c6f1a99512e12455829bfab24e
ZZZank <3410764033@qq.com>
2024-09-11 10:04:42 +0800

    ConfigBound for complex type

---
+++ src/main/java/zzzank/libs/config/impl/bound/CollectBound.java
@@ -0,0 +1,51 @@
+package zzzank.libs.config.impl.bound;
+
+import lombok.val;
+import org.jetbrains.annotations.NotNull;
+import zzzank.libs.config.api.bound.ConfigBound;
+
+import java.util.Collection;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+
+/**
+ * @author ZZZank
+ */
+public class CollectBound<T extends Collection<?>> implements ConfigBound<T> {
+    private final Supplier<T> supplier;
+    private final BiConsumer<T, Object> accumulator;
+
+    public CollectBound(Supplier<T> supplier, BiConsumer<T, Object> accumulator) {
+        this.supplier = supplier;
+        this.accumulator = accumulator;
+    }
+
+    public <E> CollectBound(Supplier<T> supplier, BiConsumer<T, E> accumulator, ConfigBound<E> elementBound) {
+        this(
+            supplier,
+            (collection, obj) -> accumulator.accept(collection, elementBound.adapt(obj))
+        );
+    }
+
+    @Override
+    public @NotNull T getDefault() {
+        return supplier.get();
+    }
+
+    @Override
+    public boolean test(Object value) {
+        return value instanceof Iterable<?>;
+    }
+
+    @Override
+    public @NotNull T adapt(Object value) {
+        val adapted = supplier.get();
+        if (!test(value)) {
+            return getDefault();
+        }
+        for (val o : (Iterable<?>) value) {
+            accumulator.accept(adapted, o);
+        }
+        return adapted;
+    }
+}
---
+++ src/main/java/zzzank/libs/config/impl/bound/MapBound.java
@@ -0,0 +1,60 @@
+package zzzank.libs.config.impl.bound;
+
+import lombok.val;
+import org.jetbrains.annotations.NotNull;
+import zzzank.libs.config.api.bound.ConfigBound;
+
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+
+/**
+ * @author ZZZank
+ */
+public class MapBound<T extends Map<?, ?>> implements ConfigBound<T> {
+    private final Supplier<T> supplier;
+    private final BiConsumer<T, Object> accumulator;
+
+    public MapBound(Supplier<T> supplier, BiConsumer<T, Object> accumulator) {
+        this.supplier = supplier;
+        this.accumulator = accumulator;
+    }
+
+    public <K, V> MapBound(
+        Supplier<T> supplier,
+        BiConsumer<T, Map.Entry<K, V>> accumulator,
+        ConfigBound<K> keyBound,
+        ConfigBound<V> valueBound
+    ) {
+        this(
+            supplier,
+            (collection, obj) -> accumulator.accept(
+                collection,
+                new AbstractMap.SimpleImmutableEntry<>(keyBound.adapt(obj), valueBound.adapt(obj))
+            )
+        );
+    }
+
+    @Override
+    public @NotNull T getDefault() {
+        return supplier.get();
+    }
+
+    @Override
+    public boolean test(Object value) {
+        return value instanceof Iterable<?>;
+    }
+
+    @Override
+    public @NotNull T adapt(Object value) {
+        val adapted = supplier.get();
+        if (!test(value)) {
+            return getDefault();
+        }
+        for (val o : (Iterable<?>) value) {
+            accumulator.accept(adapted, o);
+        }
+        return adapted;
+    }
+}
