3d90e192b6b2e30c8cf2884b292f85774f69f74b
ZZZank <3410764033@qq.com>
2024-09-12 23:42:13 +0800

    optional auto save

---
+++ src/main/java/zzzank/libs/config/impl/entry/listener/AutoSaveListener.java
@@ -0,0 +1,23 @@
+package zzzank.libs.config.impl.entry.listener;
+
+import zzzank.libs.config.api.entry.ConfigEntry;
+import zzzank.libs.config.api.entry.ConfigListener;
+
+/**
+ * @author ZZZank
+ */
+public class AutoSaveListener<T> implements ConfigListener<T> {
+
+    public static final AutoSaveListener<Object> INSTANCE = new AutoSaveListener<>();
+
+    public static <T> AutoSaveListener<T> of() {
+        return (AutoSaveListener<T>) INSTANCE;
+    }
+
+    private AutoSaveListener() {}
+
+    @Override
+    public void postSet(ConfigEntry<T> entry, T oldValue) {
+        entry.getRoot().save();
+    }
+}
