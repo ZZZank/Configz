5127c625c098ef95c3fea17a313152b853c0c528
ZZZank <3410764033@qq.com>
2024-09-12 23:04:00 +0800

    map implementation of category should be ordered

--- src/main/java/zzzank/libs/config/impl/bound/CategoryBound.java
+++ src/main/java/zzzank/libs/config/impl/bound/CategoryBound.java
@@ -2,18 +2,18 @@ package zzzank.libs.config.impl.bound;
 
 import zzzank.libs.config.api.entry.ConfigEntry;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
  * @author ZZZank
  */
-public class CategoryBound extends DefaultOnlyBound<Map<String, ConfigEntry<?>>> {
+public class CategoryBound extends DefaultBound<Map<String, ConfigEntry<?>>> {
     public static CategoryBound of() {
         return new CategoryBound();
     }
 
     public CategoryBound() {
-        super(new HashMap<>());
+        super(new LinkedHashMap<>());
     }
 }
