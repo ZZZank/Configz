9d44a9456faf02a2588779a851bbbae802de3336
ZZZank <3410764033@qq.com>
2024-09-17 01:14:26 +0800

    there should also be a default value for CollectBound

--- src/main/java/zzzank/libs/config/impl/bound/complex/CollectBound.java
+++ src/main/java/zzzank/libs/config/impl/bound/complex/CollectBound.java
@@ -3,28 +3,35 @@ package zzzank.libs.config.impl.bound.complex;
 import lombok.val;
 import org.jetbrains.annotations.NotNull;
 import zzzank.libs.config.api.bound.ConfigBound;
+import zzzank.libs.config.impl.bound.DefaultBound;
 
 import java.util.Collection;
 import java.util.function.BiConsumer;
 import java.util.function.Supplier;
+import java.util.stream.Collector;
 
 /**
  * @author ZZZank
  */
-public class CollectBound<T extends Collection<?>> implements ConfigBound<T> {
+public class CollectBound<T extends Collection<?>> extends DefaultBound<T> {
     private final Supplier<T> supplier;
     private final BiConsumer<T, Object> accumulator;
 
-    public CollectBound(Supplier<T> supplier, BiConsumer<T, Object> accumulator) {
-        this.supplier = supplier;
-        this.accumulator = accumulator;
+    public CollectBound(T defaultValue, Collector<Object, T, T> collector) {
+        super(defaultValue);
+        supplier = collector.supplier();
+        accumulator = collector.accumulator();
     }
 
-    public <E> CollectBound(Supplier<T> supplier, BiConsumer<T, E> accumulator, ConfigBound<E> elementBound) {
-        this(
-            supplier,
-            (collection, obj) -> accumulator.accept(collection, elementBound.adapt(obj))
-        );
+    public <E> CollectBound(
+        T defaultValue,
+        Supplier<T> supplier,
+        BiConsumer<T, E> accumulator,
+        ConfigBound<E> elementBound
+    ) {
+        super(defaultValue);
+        this.supplier = supplier;
+        this.accumulator = (collection, obj) -> accumulator.accept(collection, elementBound.adapt(obj));
     }
 
     @Override
