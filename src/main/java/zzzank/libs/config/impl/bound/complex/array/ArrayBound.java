36b7ed69492dcbc2eca5aeddcb79c3a20efc108d
ZZZank <3410764033@qq.com>
2024-09-20 09:26:42 +0800

    allow skipping adding value thats not adapted into array

--- src/main/java/zzzank/libs/config/impl/bound/complex/array/ArrayBound.java
+++ src/main/java/zzzank/libs/config/impl/bound/complex/array/ArrayBound.java
@@ -3,46 +3,56 @@ package zzzank.libs.config.impl.bound.complex.array;
 import lombok.val;
 import org.jetbrains.annotations.NotNull;
 import zzzank.libs.config.api.bound.ConfigBound;
-import zzzank.libs.config.impl.bound.DefaultBound;
+import zzzank.libs.config.impl.bound.UnifiedBound;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Objects;
+import java.util.Optional;
 
 /**
  * @author ZZZank
  */
-public class ArrayBound<T> extends DefaultBound<T[]> {
+public class ArrayBound<T> extends UnifiedBound<T[]> {
     private final ConfigBound<T> memberBound;
     private final T[] typeRef;
+    private final boolean skipDefault;
 
     public ArrayBound(@NotNull T[] defaultValue, @NotNull ConfigBound<T> memberBound) {
+        this(defaultValue, memberBound, false);
+    }
+
+    public ArrayBound(@NotNull T[] defaultValue, @NotNull ConfigBound<T> memberBound, boolean skipDefault) {
         super(defaultValue);
         this.memberBound = Objects.requireNonNull(memberBound);
         this.typeRef = (T[]) Array.newInstance(defaultValue.getClass().componentType(), 0);
+        this.skipDefault = skipDefault;
     }
 
     @Override
-    public boolean test(Object value) {
-        return value != null && (value instanceof Iterable<?> || value.getClass().isArray());
-    }
-
-    @Override
-    public T[] adapt(Object value) {
-        if (!test(value)) {
-            return defaultValue;
+    public Optional<T[]> tryAdapt(Object value) {
+        if (value == null) {
+            return Optional.empty();
         }
         val list = new ArrayList<T>();
         if (value instanceof Iterable<?> iterable) {
             for (val o : iterable) {
+                if (skipDefault && !memberBound.test(value)) {
+                    continue;
+                }
                 list.add(memberBound.adapt(o));
             }
+            return Optional.of(list.toArray(typeRef));
         } else if (value.getClass().isArray()) {
             val size = Array.getLength(value);
             for (int i = 0; i < size; i++) {
+                if (skipDefault && !memberBound.test(value)) {
+                    continue;
+                }
                 list.add(memberBound.adapt(Array.get(value, i)));
             }
+            return Optional.of(list.toArray(typeRef));
         }
-        return list.toArray(typeRef);
+        return Optional.empty();
     }
 }
