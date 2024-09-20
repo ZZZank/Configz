073c3f4db487424e0c6a9446809eadbe099f6668
ZZZank <3410764033@qq.com>
2024-09-21 00:06:10 +0800

    register predefined bound

--- src/main/java/zzzank/libs/config/natived/BoundTypeResolver.java
+++ src/main/java/zzzank/libs/config/natived/BoundTypeResolver.java
@@ -1,8 +1,12 @@
 package zzzank.libs.config.natived;
 
+import lombok.val;
 import zzzank.libs.config.api.bound.ConfigBound;
+import zzzank.libs.config.impl.bound.complex.array.*;
+import zzzank.libs.config.impl.bound.obj.StrBound;
 import zzzank.libs.config.impl.bound.primitive.BoolBound;
-import zzzank.libs.config.impl.bound.primitive.number.IntBound;
+import zzzank.libs.config.impl.bound.primitive.CharBound;
+import zzzank.libs.config.impl.bound.primitive.number.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,43 +19,44 @@ public class BoundTypeResolver {
     private static final Map<Class<?>, BoundConstructor> ADAPTERS = new HashMap<>();
 
     static {
-        register(boolean.class, BoolBound::new);
-        register(Boolean.class, BoolBound::new);
+        //int
         register(int.class, IntBound::new);
+        register(int[].class, IntArrayBound::new);
         register(Integer.class, IntBound::new);
-        //todo: look
-        /*
-        register(boolean.class,     TypeAdapters.bool);
-        register(boolean[].class,   TypeAdapters.boolA);
-        register(Boolean.class,     TypeAdapters.Bool);
-        register(Boolean[].class,   TypeAdapters.BoolA);
-        register(float.class,       TypeAdapters.flt);
-        register(float[].class,     TypeAdapters.fltA);
-        register(Float.class,       TypeAdapters.Flt);
-        register(Float[].class,     TypeAdapters.FltA);
-        register(double.class,      TypeAdapters.dbl);
-        register(double[].class,    TypeAdapters.dblA);
-        register(Double.class,      TypeAdapters.Dbl);
-        register(Double[].class,    TypeAdapters.DblA);
-        register(byte.class,        TypeAdapters.byt);
-        register(byte[].class,      TypeAdapters.bytA);
-        register(Byte.class,        TypeAdapters.Byt);
-        register(Byte[].class,      TypeAdapters.BytA);
-        register(char.class,        TypeAdapters.chr);
-        register(char[].class,      TypeAdapters.chrA);
-        register(Character.class,   TypeAdapters.Chr);
-        register(Character[].class, TypeAdapters.ChrA);
-        register(short.class,       TypeAdapters.shrt);
-        register(short[].class,     TypeAdapters.shrtA);
-        register(Short.class,       TypeAdapters.Shrt);
-        register(Short[].class,     TypeAdapters.ShrtA);
-        register(int.class,         TypeAdapters.int_);
-        register(int[].class,       TypeAdapters.intA);
-        register(Integer.class,     TypeAdapters.Int);
-        register(Integer[].class,   TypeAdapters.IntA);
-        register(String.class,      TypeAdapters.Str);
-        register(String[].class,    TypeAdapters.StrA);
-         */
+        register(Integer[].class, arrayCtor(IntBound.DEFAULT_ZERO));
+        register(byte.class, ByteBound::new);
+        register(byte[].class, ByteArrayBound::new);
+        register(Byte.class, ByteBound::new);
+        register(Byte[].class, arrayCtor(ByteBound.DEFAULT_ZERO));
+        register(short.class, ShortBound::new);
+        register(short[].class, ShortArrayBound::new);
+        register(Short.class, ShortBound::new);
+        register(Short[].class, arrayCtor(ShortBound.DEFAULT_ZERO));
+        register(long.class, LongBound::new);
+        register(long[].class, LongArrayBound::new);
+        register(Long.class, LongBound::new);
+        register(Long[].class, arrayCtor(LongBound.DEFAULT_ZERO));
+        //float
+        register(float.class, FloatBound::new);
+        register(float[].class, FloatArrayBound::new);
+        register(Float.class, FloatBound::new);
+        register(Float[].class, arrayCtor(FloatBound.DEFAULT_ZERO));
+        register(double.class,      DoubleBound::new);
+        register(double[].class,    DoubleArrayBound::new);
+        register(Double.class,      DoubleBound::new);
+        register(Double[].class,    arrayCtor(DoubleBound.DEFAULT_ZERO));
+        //other primitive
+        register(boolean.class, BoolBound::new);
+        register(boolean[].class, BoolArrayBound::new);
+        register(Boolean.class, BoolBound::new);
+        register(Boolean[].class, arrayCtor(BoolBound.DEFAULT_FALSE));
+        register(char.class, CharBound::new);
+        register(char[].class, CharArrayBound::new);
+        register(Character.class, CharBound::new);
+        register(Character[].class, arrayCtor(CharBound.DEFAULT_MAX));
+        //other predefined
+        register(String.class, StrBound::new);
+        register(String[].class, arrayCtor(StrBound.DEFAULT_EMPTY));
     }
 
     public static <T> void register(Class<T> type, BoundConstructor<T> boundConstructor) {
@@ -69,6 +74,10 @@ public class BoundTypeResolver {
         return ADAPTERS.containsKey(type)
             || Map.class.isAssignableFrom(type)
             || Enum.class.isAssignableFrom(type);
+    }
+
+    private static <T> BoundConstructor<T[]> arrayCtor(ConfigBound<T> memberBound) {
+        return defaultValue -> new ArrayBound<>(defaultValue, memberBound, true);
     }
 
     public interface BoundConstructor<T> {
