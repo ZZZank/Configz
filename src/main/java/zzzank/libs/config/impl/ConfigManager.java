610cdd9504f08b0d7d116665599d5d70d198aa65
ZZZank <3410764033@qq.com>
2024-09-23 12:02:28 +0800

    makes config map id based

--- src/main/java/zzzank/libs/config/impl/ConfigManager.java
+++ src/main/java/zzzank/libs/config/impl/ConfigManager.java
@@ -1,35 +1,62 @@
 package zzzank.libs.config.impl;
 
 import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import com.google.common.collect.Table;
+import org.jetbrains.annotations.NotNull;
 import zzzank.libs.config.api.entry.ConfigRoot;
 import zzzank.libs.config.natived.ConfigGenerator;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author ZZZank
  */
 public class ConfigManager {
 
+    /**
+     * name -> config root
+     */
+    private static final Map<String, ConfigRoot> ROOTS_BY_NAME = new HashMap<>();
+    /**
+     * modid -> config root
+     */
+    private static final Multimap<String, ConfigRoot> ROOTS_BY_MOD = HashMultimap.create();
+
     private static final Table<Class<?>, String, ConfigRoot> TABLE = HashBasedTable.create();
     public static final String DEFAULT_CATEGORY = "general";
 
-    public static ConfigRoot register(Class<?> target) {
-        return register(target, ConfigGenerator.scan(target));
+    public static @NotNull ConfigRoot register(Class<?> target) {
+        return register(ConfigGenerator.scan(target));
     }
 
-    public static ConfigRoot register(Class<?> identifier, ConfigRoot root) {
-        if (TABLE.contains(identifier, root.getName())) {
-            throw new IllegalStateException("already registered");
+    public static @NotNull ConfigRoot register(@NotNull ConfigRoot root) {
+        if (ROOTS_BY_NAME.containsKey(root.getName())) {
+            throw new IllegalStateException("config with name '%s' already registered".formatted(root.getName()));
         }
-        TABLE.put(identifier, root.getName(), root);
+        ROOTS_BY_NAME.put(root.getName(), root);
         return root;
     }
 
-    public static ConfigRoot get(Class<?> identifier) {
-        return TABLE.get(identifier, DEFAULT_CATEGORY);
+    public static Collection<ConfigRoot> get(String modid) {
+        return ROOTS_BY_MOD.get(modid);
     }
 
-    public static ConfigRoot get(Class<?> identifier, String category) {
-        return TABLE.get(identifier, category);
+    public static ConfigRoot getByFileName(String name) {
+        return ROOTS_BY_NAME.get(name);
+    }
+
+    /**
+     * ?
+     */
+    public static ConfigRoot get(String modid, String category) {
+        return ROOTS_BY_MOD.get(modid)
+            .stream()
+            .filter(root -> root.getName().equals(category))
+            .findAny()
+            .orElse(null);
     }
 }
